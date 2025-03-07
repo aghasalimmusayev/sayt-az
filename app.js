841298af3c4ab9b818531b9c262e9f6f4a54a6c0
Agasalim <aga77ams@gmail.com>
2025-03-07 18:13:09 +0400

    4-th commit

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 let options = document.querySelector(".options");
 let selected = document.querySelector(".selected");
+let vebsaytList = document.querySelector(".vebsayt_list");
+let domenList = document.querySelector(".domen_list");
+let hostingList = document.querySelector(".hosting_list");
+let xidmetList = document.querySelector(".xidmet_list");
+let sirketList = document.querySelector(".sirket_list");
+
+vebsaytShow();
+domenShow();
+hostingShow();
+xidmetShow();
+sirketShow();
+
 options.style.display = "none"
 selected.onclick = function(){
     if(options.style.display == "none") options.style.display ="block"
@@ -21,12 +33,36 @@ document.addEventListener("click", function(e) {
 });
 function openBar(){
     document.querySelector(".links").style.display = "initial"
+    document.querySelector(".right_links").style.display = "initial"
     document.querySelector(".fa-bars").style.display = "none"
+    document.querySelector(".fa-xmark").style.display = "initial" // Əlavə edildi
 }
 function closeBar(){
     document.querySelector(".links").style.display = "none"
+    document.querySelector(".right_links").style.display = "none"
     document.querySelector(".fa-bars").style.display = "initial"
+    document.querySelector(".fa-xmark").style.display = "none" // Əlavə edildi
 }
+document.addEventListener("click", function(event){
+    const bars = document.querySelector(".fa-bars");
+    const xmark = document.querySelector(".fa-xmark");
+    const links = document.querySelector(".links");
+    const rightLinks = document.querySelector(".right_links");
+    
+    if (!links.contains(event.target) && 
+        !rightLinks.contains(event.target) && 
+        !bars.contains(event.target) && 
+        !xmark.contains(event.target) &&
+        (links.style.display === "initial" || rightLinks.style.display === "initial")) {
+        closeBar();
+    }
+});
+document.querySelector(".fa-bars").addEventListener("click", function() {
+    openBar();
+});
+document.querySelector(".fa-xmark").addEventListener("click", function() {
+    closeBar();
+});
 document.querySelectorAll(".navlinks > li").forEach(function(item){
     item.addEventListener("click", function(event){
         event.stopPropagation(); // Klikin sənədə yayılmasının qarşısını al
@@ -41,3 +77,73 @@ document.addEventListener("click", function(event) {
         }
     });
 });
+function vebsaytShow(){
+    saytData.navLinks.vebsite.forEach(item => {
+        vebsaytList.innerHTML += `
+            <li class="py-1 px-5 hover:bg-slate-50">
+                <a href="#" class="flex items-center">
+                    <img class="mr-4 p-3 bg-blue-100 w-12 h-12 rounded-lg" src="img/hokumet-kommersiya.svg" alt="link_img">
+                    <div class="li_text">
+                        <p class="block text-slate-700 font-medium text-base sm:w-full lg:w-max lg:pr-3">${item.name}</p>
+                        <span class="block text-slate-500 md:text-sm text-xs lg:w-max">${item.name}</span>
+                    </div>
+                </a>
+            </li>`
+    })
+}
+function domenShow(){
+    saytData.navLinks.domen.forEach(item => {
+        domenList.innerHTML += `
+            <li class="py-1 px-5 hover:bg-slate-50">
+                <a href="#" class="flex items-center">
+                    <img class="mr-4 p-3 bg-blue-100 w-12 h-12 rounded-lg" src="img/hokumet-kommersiya.svg" alt="link_img">
+                    <div class="li_text">
+                        <p class="block text-slate-700 font-medium text-base sm:w-full lg:w-max lg:pr-3">${item.name}</p>
+                        <span class="block text-slate-500 md:text-sm text-xs lg:w-max">${item.name}</span>
+                    </div>
+                </a>
+            </li>`
+    })
+}
+function hostingShow(){
+    saytData.navLinks.hosting.forEach(item => {
+        hostingList.innerHTML += `
+            <li class="py-1 px-5 hover:bg-slate-50">
+                <a href="#" class="flex items-center">
+                    <img class="mr-4 p-3 bg-blue-100 w-12 h-12 rounded-lg" src="img/hokumet-kommersiya.svg" alt="link_img">
+                    <div class="li_text">
+                        <p class="block text-slate-700 font-medium text-base sm:w-full lg:w-max lg:pr-3">${item.name}</p>
+                        <span class="block text-slate-500 md:text-sm text-xs lg:w-max">${item.name}</span>
+                    </div>
+                </a>
+            </li>`
+    })
+}
+function xidmetShow(){
+    saytData.navLinks.xidmetler.forEach(item => {
+        xidmetList.innerHTML += `
+            <li class="py-1 px-5 hover:bg-slate-50">
+                <a href="#" class="flex items-center">
+                    <img class="mr-4 p-3 bg-blue-100 w-12 h-12 rounded-lg" src="img/hokumet-kommersiya.svg" alt="link_img">
+                    <div class="li_text">
+                        <p class="block text-slate-700 font-medium text-base sm:w-full lg:w-max lg:pr-3">${item.name}</p>
+                        <span class="block text-slate-500 md:text-sm text-xs lg:w-max">${item.name}</span>
+                    </div>
+                </a>
+            </li>`
+    })
+}
+function sirketShow(){
+    saytData.navLinks.sirket.forEach(item => {
+        sirketList.innerHTML += `
+            <li class="py-1 px-5 hover:bg-slate-50">
+                <a href="#" class="flex items-center">
+                    <img class="mr-4 p-3 bg-blue-100 w-12 h-12 rounded-lg" src="img/hokumet-kommersiya.svg" alt="link_img">
+                    <div class="li_text">
+                        <p class="block text-slate-700 font-medium text-base sm:w-full lg:w-max lg:pr-3">${item.name}</p>
+                        <span class="block text-slate-500 md:text-sm text-xs lg:w-max">${item.name}</span>
+                    </div>
+                </a>
+            </li>`
+    })
+}
---
+++ data.js
@@ -0,0 +1,40 @@
+let saytData = 
+    {navLinks: {
+        vebsite: [
+            { name: "Saytlarin hazirlanmasi", tesvir: "Veb saytlarin hazirlanmasi"},
+            { name: "Hokumet ve qeyri-kommersiya saytlari", tesvir: "Hokumet ve qeyri-kommersiya saytlari"},
+            { name: "Kicik biznes saytlari", tesvir: "Kicik biznesler ucun saytlar"},
+            { name: "E-ticaret saytlari", tesvir: "E-ticaret saytlari"},
+            { name: "Huquq firma sayti", tesvir: "Huquq firma sayti"},
+            { name: "Bloqlar ve Sexsi Vebsayt", tesvir: "Bloqlar ve Sexsi Vebsayt"},
+            { name: "Dasinmaz emlak veb sayti", tesvir: "Dasinmaz emlak veb sayti"},
+            { name: "Tibb - Doctor veb Saytlar", tesvir: "Tibb Doctor Saytlar"},
+            { name: "WordPress Saytlari", tesvir: "WordPress Saytlari"}
+        ],
+        domen: [
+            {name: "Domen", tesvir:"Etibarlı xidmət"},
+            {name: "Pulsuz Domen Qeydiyyatı", tesvir:"Sürətli qeydiyyat"}
+        ],
+        hosting: [
+            {name:" Veb Hosting", tesvir:"₼10.00/ay-dab başlayaraq"},
+            {name:" VPS", tesvir:"₼15.00/ay-dab başlayaraq"},
+            {name:" Server", tesvir:"₼99.00/ay-dab başlayaraq"}
+        ],
+        xidmetler: [
+            {name: "Vebsayt sifarişinizi izləyin", tesvir: "Sifarişinizi asanlıqla izləyin"},
+            {name: "Sayt Kalkulyatoru", tesvir: "Saytın qiymətini öyrən"},
+            {name: "SEO Xidməti", tesvir: "Axtarış sistemi optimizasiyası"},
+            {name: "Mobil Tətbiqlərin hazırlanması", tesvir: "Mobil tətbiq xidməti"},
+            {name: "Texniki Xidmət", tesvir: "Texniki Xidmət"},
+            {name: "Sosial Media Marketinq", tesvir: "Sosial Media Marketinq"}
+        ],
+        sirket: [
+            {name: "Haqqımızda", tesvir: "Bizim komanda üzvlərimiz"},
+            {name: "Bloq", tesvir: "Ən son xəbərlər və faydalı məlumatlar"},
+            {name: "Bütün StartUplar", tesvir: "Startupları idarə edən komandamıza qoşulun"},
+            {name: "Portfolio", tesvir: "Hazırladığımız hər bir iş bizim üçün dəyərlidir"},
+            {name: "Karyera", tesvir: "Hazıkı vakansiyalarımız"},
+            {name: "Freelance", tesvir: "Siz də qeydiyyatdan keçməklə freelance işləyə bilərsiz"},
+            {name: "Əlaqə Forması", tesvir: "Əlaqə saxlayın"}
+        ]}
+    }
